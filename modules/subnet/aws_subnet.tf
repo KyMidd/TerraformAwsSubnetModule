67c4a14e80e75cd34e4cf178cf9b350c37fe08bd
Kyler Middleton <kymidd@gmail.com>
2019-11-10 13:04:34 -0600

    fix availability zone assign

--- modules/subnet/aws_subnet.tf
+++ modules/subnet/aws_subnet.tf
@@ -1,7 +1,12 @@
+# Find the current AWS region to use for availability zone
+data "aws_region" "current" {}
+
+# Build subnet resource
 resource "aws_subnet" "subnet" {
   count      = length(var.subnet_addresses)
   vpc_id     = var.vpc_id
   cidr_block = element(var.subnet_addresses, count.index)
+  availability_zone = "${data.aws_region.current.name}${element(var.availability_zone, count.index)}"
 
   tags = {
     Terraform = "true"
